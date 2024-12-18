4e6526a61bd42930e8c6c3853ece0ce31824d5db
Suchit G <suchitg04@gmail.com>
2024-12-18 20:51:48 +0530

    fix: protect inline route

--- src/routes/inlineChatRoutes.js
+++ src/routes/inlineChatRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import getEditedCode from '../controllers/inlineChatController.js';
+import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router()
 
-router.post('/', getEditedCode)
+router.post('/', protect, getEditedCode)
 
 export default router;
