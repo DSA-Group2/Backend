a5d672381cd5f79b2bb78b8c8dc68600ab35299d
Mohit V S <vsmohit94@gmail.com>
2024-12-19 10:36:26 +0530

    middleware update

--- src/routes/codeRoutes.js
+++ src/routes/codeRoutes.js
@@ -1,14 +1,21 @@
-import express from 'express';
-import { executeCode, saveCode, getCodeHistory, editCode, deleteCode, getProgramById } from '../controllers/codeController.js';
-import protect from '../middleware/authMiddleware.js';
+import express from "express";
+import {
+    executeCode,
+    saveCode,
+    getCodeHistory,
+    editCode,
+    deleteCode,
+    getProgramById,
+} from "../controllers/codeController.js";
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/execute', protect, executeCode);
-router.post('/save', protect, saveCode);
-router.get('/history/:userId', protect, getCodeHistory);
-router.put('/save/:id', protect, editCode); 
-router.delete('/delete/:id', protect, deleteCode);
-router.get('/program/:id', protect, getProgramById);
+router.post("/execute", executeCode);
+router.post("/save", protect, saveCode);
+router.get("/history/:userId", protect, getCodeHistory);
+router.put("/save/:id", protect, editCode);
+router.delete("/delete/:id", protect, deleteCode);
+router.get("/program/:id", protect, getProgramById);
 
 export default router;
